0b255090dde8a1e870ab1379b51bd2beb8399375
Nasir Uddin <nasiruddin3954@gmail.com>
2025-02-26 19:00:07 +0600

    find

---
+++ Module_11/find_using_loop.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int leader[1005];
+
+int find(int val)
+{
+    while (leader[val] != -1)
+    {
+        cout << val << endl;
+        val = leader[val];
+    }
+
+    return val;
+}
+int main()
+{
+    memset(leader, -1, sizeof(leader));
+    leader[0] = 1;
+    leader[1] = -1;
+    leader[2] = 1;
+    leader[3] = 1;
+    leader[4] = 5;
+    leader[5] = 3;
+
+    // for (int i = 0; i < 6; i++)
+    //     cout << leader[i] << " ";
+    cout << find(4) << endl;
+    return 0;
+}
--- Module_11/find_using_recursion.cpp
+++ Module_11/find_using_recursion.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int leader[1005];
 
 int find(int val)
 {
-    while(leader[val] != -1)
-    {
-        cout << val << endl;
-        val = leader[val];
-    }
-   
-
-    return val;
+    cout << val << endl;
+    if (leader[val] == -1)
+        return val;
+    return find(leader[val]);
+  
 }
 int main()
 {
-    memset(leader,-1,sizeof(leader));
+    memset(leader, -1, sizeof(leader));
     leader[0] = 1;
     leader[1] = -1;
     leader[2] = 1;
     leader[3] = 1;
     leader[4] = 5;
     leader[5] = 3;
-  
+
+   
     cout << find(4) << endl;
     return 0;
 }
