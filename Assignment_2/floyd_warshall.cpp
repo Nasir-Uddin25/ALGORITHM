5109c01fac69a8807dc217c6d7f6b748cf530369
Nasir Uddin <nasiruddin3954@gmail.com>
2025-03-02 00:09:35 +0600

    floyd

--- Assignment_2/floyd_warshall.cpp
+++ Assignment_2/floyd_warshall.cpp
@@ -1,8 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    
+    int n, e;
+    cin >> n >> e;
+
+    int adj_mat[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                adj_mat[i][j] = 0;
+            else
+                adj_mat[i][j] = INT_MAX;
+        }
+    }
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_mat[a][b] = c;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         if (adj_mat[i][j] == INT_MAX)
+    //             cout << "INF" << " ";
+    //         else
+    //             cout << adj_mat[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
     return 0;
 }
